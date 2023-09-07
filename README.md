281644864a11cd2f9f2588a4c7a4a0e57c304208
Wessel Vonk <wjc.vonk@gmail.com>
2023-09-07 10:43:42 +0200

    Update factory.py

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 Simplify the interaction with the YouTube API.
 
 ## Usage
-It's convinient to `export YOUTUBE_API_KEY=...`.
+It's convinient to:
+
+```sh
+export YOUTUBE_API_KEY=...
+export YOUTUBE_CLIENT_SECRETS_FILE=...
+```
--- src/chootube/factory.py
+++ src/chootube/factory.py
@@ -1,16 +1,38 @@
-from os import getenv
+import os
+from pathlib import Path
 
+import google_auth_oauthlib  # type: ignore[import]
+from google.oauth2.credentials import Credentials  # type: ignore[import]
 from googleapiclient.discovery import Resource, build
 
 YOUTUBE: Resource | None = None
 
+KWARGS_DEFAULT = {"serviceName": "youtube", "version": "v3"}
+KWARGS_FROM_ENV = {"developerKey": os.getenv("YOUTUBE_API_KEY")}
 
-def make_youtube(api_key: str | None = None) -> Resource:
+
+def make_credentials_from_client_secrets_file(
+    filepath: str | Path | None = None, scopes: list[str] = []
+) -> Credentials:
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    filepath = filepath or os.getenv("YOUTUBE_CLIENT_SECRETS_FILE")
+    if not filepath:
+        raise Exception("No proper 'filepath' was specified.")
+
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        filepath, scopes
+    )
+
+    return flow.run_local_server()
+
+
+def make_youtube(force: bool = False, **kwargs) -> Resource:
     global YOUTUBE
 
-    if not YOUTUBE:
-        YOUTUBE = build(
-            "youtube", "v3", developerKey=api_key or getenv("YOUTUBE_API_KEY")
-        )
+    kwargs_from_merger = {**KWARGS_DEFAULT, **KWARGS_FROM_ENV, **kwargs}
+
+    if force or not YOUTUBE:
+        YOUTUBE = build(**kwargs_from_merger)
 
     return YOUTUBE
